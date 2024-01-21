fab1d3bda8c9b6d226021576139184b9cac24bc4
mugeon <pos04167@kakao.com>
2024-01-21 18:26:18 +0900

    feat:NotionAPiResponse 반환값 변경

--- src/main/java/com/example/dailyworknotion/controller/NotionController.java
+++ src/main/java/com/example/dailyworknotion/controller/NotionController.java
@@ -1,6 +1,6 @@
 package com.example.dailyworknotion.controller;
 
-import com.example.dailyworknotion.controller.dto.PageListResponseDTO;
+import com.example.dailyworknotion.controller.dto.NotionApiResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,8 +16,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
 import java.util.Collections;
-
-import static java.lang.String.format;
 
 @RestController
 @RequestMapping(value = "/notion")
@@ -44,16 +42,23 @@ public class NotionController {
         this.objectMapper = objectMapper;
     }
 
-    //todo : 1. expcetion 처리 / 2. 엔티티 모델링 / 3.
+    /**
+     * todo
+     * 1. Record로 변경하기
+     * 2. 도메인 변경하기
+     */
+
     @GetMapping("/database")
-    public ResponseEntity<PageListResponseDTO> getNotionDatabase(HttpServletRequest request) {
+    public ResponseEntity<NotionApiResponse> getNotionDatabase(HttpServletRequest request) {
         logger.info("[" + request.getRemoteAddr() + "] ====== /notion/database [" + getClass().getSimpleName() + ".getNotionDatabase()] start ======");
+
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
 
         headers.setBearerAuth(NOTION_SECRET);
         headers.add(NOTION_VERSION_KEY, NOTION_VERSION_VALUE);
+
         logger.info("[" + request.getRemoteAddr() + "] ====== /notion/database [" + getClass().getSimpleName() + ".getNotionDatabase()] notion_secret : {}" , NOTION_SECRET);
         logger.info("[" + request.getRemoteAddr() + "] ====== /notion/database [" + getClass().getSimpleName() + ".getNotionDatabase()] NOTION_VERSION_KEY : {}" , NOTION_VERSION_KEY);
         logger.info("[" + request.getRemoteAddr() + "] ====== /notion/database [" + getClass().getSimpleName() + ".getNotionDatabase()] NOTION_VERSION_VALUE : {}" , NOTION_VERSION_VALUE);
@@ -68,11 +73,11 @@ public class NotionController {
 
         HttpEntity<String> requestEntity = new HttpEntity<>(headers);
 
-        ResponseEntity<String> result = restTemplate.postForEntity(uri, requestEntity, String.class);
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, requestEntity, String.class);
 
         try {
-            PageListResponseDTO pageListResponseDTO = objectMapper.readValue(result.getBody(), PageListResponseDTO.class);
-            return ResponseEntity.ok(pageListResponseDTO);
+            NotionApiResponse responseDTO = objectMapper.readValue(response.getBody(), NotionApiResponse.class);
+            return ResponseEntity.ok(responseDTO);
         } catch (JsonProcessingException e) {
             logger.error("DTO 매핑에 실패를 하였습니다", e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
