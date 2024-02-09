606d0aae1bd6629f7254cbe38d9a896c583282ef
mugeon <pos04167@kakao.com>
2024-02-09 20:58:19 +0900

    docs : @data 변경 @getter

--- src/main/java/com/example/dailyworknotion/controller/dto/notion/TitleProperty.java
+++ src/main/java/com/example/dailyworknotion/controller/dto/notion/TitleProperty.java
@@ -1,12 +1,11 @@
 package com.example.dailyworknotion.controller.dto.notion;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public  class TitleProperty {
